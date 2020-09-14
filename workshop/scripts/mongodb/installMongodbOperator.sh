434f596aac7298f1514eb284089458c4e5b53bf6
rojanjose <rojanjose@gmail.com>
2020-09-14 12:55:17 -0400

    Revise instructions for new release

--- workshop/scripts/mongodb/installMongodbOperator.sh
+++ workshop/scripts/mongodb/installMongodbOperator.sh
@@ -6,11 +6,17 @@ echo "Creating project for MongoDB install"
 oc new-project $PROJECT
 
 echo "Creating Operator group..." 
-oc apply -f mongodbOperatorGroup.yaml
+curl -sL https://raw.githubusercontent.com/IBM/red-hat-marketplace/master/workshop/scripts/mongodb/mongodbOperatorGroup.yaml | oc apply -f -
+#oc apply -f mongodbOperatorGroup.yaml
 
 echo "Creating subscription..." 
-oc apply -f mongodbSubscription.yaml
+curl -sL https://raw.githubusercontent.com/IBM/red-hat-marketplace/master/workshop/scripts/mongodb/mongodbSubscription.yaml | oc apply -f -
+#oc apply -f mongodbSubscription.yaml
 
 echo "Mongodb install initiated." 
-echo "Run the commands below to ensure the " 
+echo "Run the commands below to ensure the status shows ... " 
+echo "oc describe sub mongodb-enterprise-advanced-ibm-rhmp -n $PROJECT | grep -A5 Conditions"
 oc describe sub mongodb-enterprise-advanced-ibm-rhmp -n $PROJECT | grep -A5 Conditions
+
+#To run this script
+#source <(curl -s https://raw.githubusercontent.com/IBM/red-hat-marketplace/master/workshop/scripts/mongodb/installMongodbOperator.sh)
